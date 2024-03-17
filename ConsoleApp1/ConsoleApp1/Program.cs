3270e0fab46b5e29f064c028b10f02904f4746d7
unknown <s27002@pjwstk.edu.pl>
2024-03-17 23:37:19 +0100

    Function added 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,3 +5,13 @@ Console.WriteLine("Hello, Worldd!");
 Console.WriteLine("Hello, Worlddd!");
 Console.WriteLine("Hello, Worldddd!");
 Console.WriteLine("Hello, Worlddsdsddd!");
+
+int[] num = {1, 2, 3, 4, 5};
+Console.WriteLine(CalculateAverage(num));
+
+double CalculateAverage(int[] t) {
+    int sum = 0;
+    foreach(int num in t) 
+        sum += num; 
+    return (double)sum/t.Length;
+}
